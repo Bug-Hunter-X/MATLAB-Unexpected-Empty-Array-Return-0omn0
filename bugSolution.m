f95c56cc10060aa24f142f29f7bde03a35af9d70
BugHunter-X <askzenai@gmail.com>
2024-12-21 21:28:06 +0100

    Add bugSolution.m file

---
+++ bugSolution.m
@@ -0,0 +1,19 @@
+function result = myFunction(input)
+  % Some code here...
+  if someCondition
+    result = someValue;
+  else
+    result = []; % Empty array
+  end
+  % More code here...
+end
+
+% Calling the function and checking for empty array
+output = myFunction(someInput);
+if isempty(output)
+  disp('Output is empty!');
+  % Handle the empty array case appropriately, for example:
+  result = zeros(1,1); % or any other default value
+else
+  disp('Output is not empty!');
+end
